67723999af16a045137e31f65ba4be8201f45e84
Aris Yang <arisyang1981@outlook.com>
2023-06-26 08:28:47 -0400

    Replace bucket-name in main.tf using variables.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {}
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "s3-bucket-from-tf"
+  bucket = var.bucket_name
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
