ddc40df7d90cec3521452d3b973c530c1348a9b9
Aris Yang <arisyang1981@outlook.com>
2023-06-27 20:41:42 -0400

    Random bucket name instead of hardcode name.

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
 provider "aws" {}
 
+data "terraform_remote_state" "random_name" {
+  backend = "local"
+
+  config = {
+    path = "../learn-terraform-random/terraform.tfstate"
+  }
+}
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.bucket_name
+  bucket = data.terraform_remote_state.random_name.outputs.random_name_str
+  #bucket = var.bucket_name
   #bucket                   = "s3-bucket-tf"
 
   control_object_ownership = true
